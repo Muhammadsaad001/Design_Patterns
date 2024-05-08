550244d1620373b5ad86ac4e93800665a6016723
Muhammadsaad001 <115546231+Muhammadsaad001@users.noreply.github.com>
2024-05-08 12:23:39 +0500

    Add files via upload
    builder pattern complete code

--- BuilderPatterms1/src/builderpatterms1/BuilderPatterms1.java
+++ BuilderPatterms1/src/builderpatterms1/BuilderPatterms1.java
@@ -2,19 +2,41 @@ package builderpatterms1;
 
 public class BuilderPatterms1 {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Director director = new Director();
 
-        CarBuilder builder = new CarBuilder();
-        director.constructSportsCar(builder);
+        // Build a sports car and its manual
+        CarBuilder sportsCarBuilder = new CarBuilder();
+        director.constructSportsCar(sportsCarBuilder);
+        Manual sportsCar = sportsCarBuilder.getResult();
+        System.out.println("Sports Car built:\n" + sportsCar.getType());
 
-        Manual car = builder.getResult();
-        System.out.println("Car built:\n" + car.getType());
+        CarManualBuilder sportsCarManualBuilder = new CarManualBuilder();
+        director.constructSportsCar(sportsCarManualBuilder);
+        Manual sportsCarManual = sportsCarManualBuilder.getResult();
+        System.out.println("\nSports Car Manual built:\n" + sportsCarManual.print());
 
-        CarManualBuilder manualBuilder = new CarManualBuilder();
-        director.constructSportsCar(manualBuilder);
-        Manual carManual = manualBuilder.getResult();
-        System.out.println("\nCar manual built:\n" + carManual.print());
+        // Build a city car and its manual
+        CarBuilder cityCarBuilder = new CarBuilder();
+        director.constructCityCar(cityCarBuilder);
+        Manual cityCar = cityCarBuilder.getResult();
+        System.out.println("\nCity Car built:\n" + cityCar.getType());
+
+        CarManualBuilder cityCarManualBuilder = new CarManualBuilder();
+        director.constructCityCar(cityCarManualBuilder);
+        Manual cityCarManual = cityCarManualBuilder.getResult();
+        System.out.println("\nCity Car Manual built:\n" + cityCarManual.print());
+
+        // Build an SUV and its manual
+        CarBuilder suvBuilder = new CarBuilder();
+        director.constructSUV(suvBuilder);
+        Manual suv = suvBuilder.getResult();
+        System.out.println("\nSUV built:\n" + suv.getType());
+
+        CarManualBuilder suvManualBuilder = new CarManualBuilder();
+        director.constructSUV(suvManualBuilder);
+        Manual suvManual = suvManualBuilder.getResult();
+        System.out.println("\nSUV Manual built:\n" + suvManual.print());
     }
 
 }
